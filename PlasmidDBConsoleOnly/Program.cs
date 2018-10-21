7dc4c5fbe5ee630489952ad52799931f4f65b81f
Hendrik Welsch <hendrikwelsch@gmx.de>
2018-10-21 17:40:47 +0200

    Append- und Read-Funktion hinzugefügt

--- PlasmidDBConsoleOnly/Program.cs
+++ PlasmidDBConsoleOnly/Program.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.IO;
 
 namespace PlasmidDBConsoleOnly
 {
@@ -12,11 +13,22 @@ namespace PlasmidDBConsoleOnly
         {
             Console.WriteLine("Plasmiddatenbank V1.0 Konsolenversion");
             Console.WriteLine("Plasmidname eingeben: ");
-            string plasmidName=Console.ReadLine();
+            string plasmidName = Console.ReadLine();
+
+            string filePath = $"C:\\Users\\Hendrik\\Desktop\\Plasmiddatenbank\\plasmid_{plasmidName}.text";
+
             Console.WriteLine("Plasmidsequenz eingeben: ");
             string plasmidSequenz = Console.ReadLine();
 
-            Console.WriteLine(plasmidName + " " + plasmidSequenz);
+
+
+            File.AppendAllText(filePath, plasmidName);
+            File.AppendAllText(filePath, Environment.NewLine);
+            File.AppendAllText(filePath, plasmidSequenz);
+
+            string[] plasmid = File.ReadAllLines(filePath);
+
+            Console.WriteLine("Name: " + plasmid[0] + ", Sequenz: " + plasmid[1]);
             Console.ReadLine();
         }
     }
